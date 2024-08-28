7f4573774b045ba99c31cb5a5b8289dda05d9e9c
Ishan-2003 <21BCS022@nith.ac.in>
2024-08-28 08:05:51 +0530

    build-setup

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const path = require('path');
 app.use(cors());
 
+var __dirname = path.resolve()
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -17,26 +19,26 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (username,roomID) => {
+  socket.on("join_room", (username, roomID) => {
     socket.join(roomID);
-    socket.to(roomID).emit("notification",{
-      user : username,
-      room : roomID,
+    socket.to(roomID).emit("notification", {
+      user: username,
+      room: roomID,
     });
     // console.log(`User joined room: ${r}`);
   });
-  socket.on("readytoLeave",(username,roomID)=>{
-    socket.to(roomID).emit("notify",{
-      user : username,
-      room : roomID,
+  socket.on("readytoLeave", (username, roomID) => {
+    socket.to(roomID).emit("notify", {
+      user: username,
+      room: roomID,
     });
     socket.disconnect();
 
   });
 
-  socket.on("send_message",(data)=>{
-    console.log("data recieved on server",data);
-    socket.to(data.roomID).emit("receive_message",data);
+  socket.on("send_message", (data) => {
+    console.log("data recieved on server", data);
+    socket.to(data.roomID).emit("receive_message", data);
   })
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
@@ -44,6 +46,11 @@ io.on("connection", (socket) => {
 
 
 });
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 server.listen(3001, () => {
-  console.log("SERVER RUNNING")})
+  console.log("SERVER RUNNING")
+})
